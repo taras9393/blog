a50e7209d6f24de5c867b86adf93c0f0b1e226be
ttershak <ttershak@gmail.com>
2023-09-26 15:11:33 +0300

    Some bugs fixed

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fill: [900, 400]
+  process resize_to_fill: [900, 600]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
