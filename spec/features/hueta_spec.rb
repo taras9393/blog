ba1bc49ac639e217d09d0051be3bcf16c25dacc2
ttershak <ttershak@gmail.com>
2023-10-13 14:37:44 +0300

    db fix

--- spec/features/hueta_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-#describe CategoriesController, type: :controller do
-#    subject { post :create, :params => { :category => { :name => "Nature" } } }
-
-#    it "redirects_to :action => :index" do
-#      expect(subject).to redirect_to :action => :index
-#    end
-#  end
